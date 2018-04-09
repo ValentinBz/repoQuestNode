93504d4d586676895409ac03f3ecc404f5c1935c
ValentinBz <valentin.bouzin24@gmail.com>
2018-04-09 20:17:35 +0200

    upload un fichier

--- routes/upload.js
+++ routes/upload.js
@@ -9,15 +9,22 @@ router.get('/', function(req, res, next) {
 		res.sendFile(path.join(__dirname, '../views', 'upload.html'));
 });
 
-router.post('/', upload.array('monfichier', 2), function (req, res, next) {
+router.post('/', upload.array('monfichier'), function (req, res, next) {
+	console.log(req.files);
 	for (let i = 0; i < 2; i++) {
-		fs.rename(req.files[i].path, 'public/images/' + req.files.originalname, function(err){
-	    if (err) {
-	        console.log('problème durant le déplacement'); 
-	    } else {
-	        console.log('Fichier uploadé avec succès'); 
-	    }
-		})
+		if(req.files[i].mimetype !== 'image/png') {
+				console.log('error mimetype fichier n°', i);
+		}	else if (req.files[i].size > 3000000) {
+				console.log('error size fichier n°', i);
+		} else {
+			fs.rename(req.files[i].path, 'public/images/' + req.files.originalname, function(err){
+		    if (err) {
+		        console.log('problème durant le déplacement'); 
+		    } else {
+		        console.log('Fichier uploadé avec succès'); 
+		    }
+			})
+		}
 	}
 });
 
